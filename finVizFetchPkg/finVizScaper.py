009e2490845f48ca647d52d0640cb967ca3b05a6
p-hiroshige <76498713+p-hiroshige@users.noreply.github.com>
2021-08-11 09:02:18 -0700

    cleared global dataframe structures

--- finVizFetchPkg/finVizScaper.py
+++ finVizFetchPkg/finVizScaper.py
@@ -1,11 +1,15 @@
+# finviz scraping class
+#TODO imports to init.py?
+
 class finvizStreamer():
     #Declare class variables
     #---------------------
     #declare dictionary (key:value)  to store ticker and associated html tables
-    news_tables = {}
+    news_tables = {} 
+    
     #declare list (array) to store arrays of parsed data
-    parsed_data = []
-    tickers = []
+    parsed_data = [] 
+    tickers = [] 
     #---------------------
     #constructor
     def __init__(self):
@@ -18,14 +22,20 @@ class finvizStreamer():
         import json
         from bs4 import BeautifulSoup
         import pandas as pd
-
-        symbol = input('What is the symbol of the stock? (Please enter only one.)')
+        
+        symbol = input('What is the stock symbol? (Please enter only one.)')
 
         finviz_url = 'https://www.finviz.com/quote.ashx?t='
-        #tickers = ['NVDA', 'SLV', 'MU']
+        
+        #print('tickers : ', tickers)
+        
         tickers = [symbol]
+        
+        #print('tickers : ', tickers)
+        #print('self.parsed_data : ', self.parsed_data)
 
         for ticker in tickers:
+            print('ticker inside for loop: ', ticker)
             url = self.url + ticker
 
             req = Request(url = url, headers = {'user-agent': 'my-app'})
@@ -35,12 +45,12 @@ class finvizStreamer():
             html = BeautifulSoup(response, 'html')
             news_table = html.find(id = 'news-table')
             self.news_tables[ticker] = news_table
-    
 
         for ticker, news_table in self.news_tables.items():
             for row in news_table.findAll('tr'):
 
                 title = row.a.get_text()
+                link = row.a.get('href') #captures the link
                 date_data = row.td.text.split(' ')
 
                 if len(date_data) == 1: # if there is both a date and time it parses them into two columns
@@ -48,7 +58,14 @@ class finvizStreamer():
                 else: 
                     date = date_data[0] 
                     time = date_data[1]
-                self.parsed_data.append([ticker, date, time, title])
+                #print(ticker) #shows all of the ticker symbols in news_table
+                self.parsed_data.append([ticker, date, time, title, link])
+         
+        df = pd.DataFrame(self.parsed_data, columns = ['ticker', 'date', 'time', 'title', 'link'])  
+
+        self.news_tables.clear() #need to clear global dictionaries or data will be retained for each run
+        self.parsed_data.clear() #need to clear global dictionaries or data will be retained for each run
         
-        df = pd.DataFrame(self.parsed_data, columns = ['ticker', 'date', 'time', 'title'])  
+        #del news_table #news_table is storing all of the previous runs so it must be deleted
+    
         return df
--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="finVizFetchPkg",
-    version="0.0.1",
+    version="0.0.2",
     author="Example Author",
     author_email="author@example.com",
     description="A package to fetch finViz data",
